b213df951edeb551af294940f768d1ba487dd481
dgordon0 <dgordon0@student.ubc.ca>
2025-10-28 09:57:55 -0700

    Initial commit

---
+++ R/square.R
@@ -0,0 +1,10 @@
+#' @export
+# This is telling R to include this function in the Package -- R studio will handle everything
+# ' #' @ export ' Will save this function to name space
+# If you get an error --> run library(devtools)
+
+square <- function(x) {
+
+  return(x^2) #square the input and return it
+
+}
